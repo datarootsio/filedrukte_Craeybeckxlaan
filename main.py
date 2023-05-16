cf5e1f1f9b3417353387c01028644a45704caf7b
Lode Nachtergaele <you@example.comlode.nachtergaele@gmail.com>
2023-05-16 12:14:15 +0200

    better cookie, local timezone

--- main.py
+++ main.py
@@ -2,11 +2,10 @@
 
 import logging
 import logging.handlers
-import os
 from datetime import datetime, timezone
+import pytz
 from pathlib import Path
 from playwright.sync_api import sync_playwright
-
 
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
@@ -26,20 +25,21 @@ if __name__ == "__main__":
     dst = Path("./shots")  # Directory to store resulting png in.
     dst.mkdir(exist_ok=True)
     url = "https://www.google.com/maps/@50.9503538,4.7123571,734m/data=!5m1!1e1"
+    consent_url = f"https://consent.google.com/m?continue={url}"
     with sync_playwright() as p:
         browser_type = p.chromium
         browser = browser_type.launch()
-        page = browser.new_page()
-        page.goto(url)
-        for i, el in enumerate(page.get_by_label("Accept all").all()):
-            try:
-                el.click()
-            except Exception as e:
-                logging.debug(f"Clicked {i}th element: {str(e)}")
-        page.wait_for_load_state("networkidle")
+        browser = playwright.chromium.launch(headless=True)
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto(consent_url)
+        page.get_by_role("button", name="Accept all").click()
+
         now_utc = datetime.now(timezone.utc)
-        timestr = now_utc.strftime("%Y%m%d-%H%M%S")
+        now_local = now_utc.astimezone(pytz.timezone("Europe/Brussel"))
+        timestr = now_local.strftime("%Y%m%d-%H%M%S")
         shot_file = dst / f"rotselaar_{timestr}.png"
         page.screenshot(path=shot_file.as_posix())
         logger.info(f"Took shot {shot_file.as_posix()} on {timestr}.")
+        context.close()
         browser.close()
