b6405beb5d7cb068930bc8091008342695d44694
G-Donkey <100077879+G-Donkey@users.noreply.github.com>
2023-11-20 17:50:26 +0100

    Update main.py
            for url in urls:
                page.goto(url)

--- main.py
+++ main.py
@@ -46,8 +46,7 @@ def main():
         page = context.new_page()
 
         # Go through each URL and take a screenshot
-        for streetname in config[location]:
-            url = config[location][streetname]["url"]
+        for url in URLS:
             page.goto(url)
             # Replace 'Alles accepteren' with the text of the button to accept cookies in your language
             for i, el in enumerate(page.get_by_label("Alles accepteren").all()):
